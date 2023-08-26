bcd9d34a96ec1b3aa4d6f0e833a19bd8e8a366c8
jose hernandez <joseo3266@gmail.com>
2023-08-26 11:13:26 -0600

    Programa reiniciado/ mismo codigo

--- Practicing/Practicing/Program.cs
+++ Practicing/Practicing/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+//tuple elemen names
 
-namespace Practicing
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+/*(string Name, int Points, int Level) score = ("R2-D2", 12420, 15);
+System.Console.WriteLine($"Name: {score.Name} Level: {score.Level} Score: {score.Points} ");
+
+(string Name, int, int) score = ("R2-D2", 12430, 15);
+System.Console.WriteLine($"Name: {score.Name} Level: {score.Item3} Points: {score.Item2}");
+
+var score = (Name: "R2-D2", Points: 12420, Level: 15);
+Console.WriteLine($"Name: {score.Name}, Points: {score.Points}, Level: {score.Level}");
+
+(string, int P, int L) score = (Name: "R2-D2", Points: 12421, Level: 13);
+Console.WriteLine($"Name: {score.Item1}, Level: {score.L}, Points: {score.P}");
+
+
+(string Name, int Points, int Level) GetScore() => ("R2-D2", 12430, 15);
+var score = GetScore();
+Console.WriteLine($"Name: {score.Name}, Points: {score.Points}, Level: {score.Level}");
+*/
+
+(int, int) a = (1, 2);
+(int, int) b = (1, 2);
+
+Console.WriteLine(a == b);
+Console.WriteLine(a != b);
