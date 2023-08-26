97f7dafce2ce95d8f6e92b709e92239625e83773
jose hernandez <joseo3266@gmail.com>
2023-08-26 11:09:21 -0600

    eliminado archivo cs

---
+++ Practicing/Practicing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practicing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
