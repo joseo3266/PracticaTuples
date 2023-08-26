975ac743d4b0b79230992872c218ef62b14dbd2b
jose hernandez <joseo3266@gmail.com>
2023-08-26 09:59:43 -0600

    First commit

---
+++ practice_tuples.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//tuple elemen names
+
+/*(string Name, int Points, int Level) score = ("R2-D2", 12420, 15);
+System.Console.WriteLine($"Name: {score.Name} Level: {score.Level} Score: {score.Points} ");
+
+(string Name, int, int) score = ("R2-D2", 12430, 15);
+System.Console.WriteLine($"Name: {score.Name} Level: {score.Item3} Points: {score.Item2}");
+
+var score = (Name: "R2-D2", Points: 12420, Level: 15);
+Console.WriteLine($"Name: {score.Name}, Points: {score.Points}, Level: {score.Level}");
+
+(string, int P, int L) score = (Name: "R2-D2", Points: 12421, Level: 13);
+Console.WriteLine($"Name: {score.Item1}, Level: {score.L}, Points: {score.P}");
+*/
